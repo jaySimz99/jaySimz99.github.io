6110fdc4a9b5ff676528b100e2ac43e72a1077b7
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2021-08-12 10:43:50 +0100

    32

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,37 @@
+const   cache_name = 'v1';
+const assets = [
+  '/',
+  'index.html',
+  'gameloop.html',
+  'plushyPink.css',
+  'perfectPurple.css',
+  'style.css',
+  'script.js',
+  'gameplay.js',
+  'gameMultiplayer.js',
+  'localStorage.js',
+  'manifest.json',
+  'rock.png',
+  'paper.png',
+  'scissors.png',
+  'stars.png',
+  'starsfilled.png',
+  'load.png',
+];
+
 
 self.addEventListener('install', (e) => {
 console.log('Service Worker: installed');
+
+e.waitUntil(
+caches.open(cache_name).then(cache => {console.log('service work: caching files');
+cache.addAll(assets);
+}).then(() => self.skipWaiting())
+);
 });//call installation 
 
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: activated');
+  });//call activation
+  
