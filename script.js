f4f698fb318ad02f72338b2ebc83c60a68e7b8b2
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2021-08-12 09:40:23 +0100

    30

--- script.js
+++ script.js
@@ -61,3 +61,14 @@ window.onresize = function() {
 	viewport_set(); 
 }
 
+
+
+if ('serviceWorker' in navigator) {
+    console.log('service worker works');
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(reg => console.log('service worker registered'))
+      .catch(err => console.log('Error: ${err}')) 
+    })
+}
